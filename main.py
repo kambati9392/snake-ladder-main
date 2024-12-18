327b49616571d387e8a26d49d7aedc0449043da8
Rammohan <reddy.kambati@gmail.com>
2024-12-18 14:28:17 +0530

    added main.py

--- main.py
+++ main.py
@@ -4,25 +4,27 @@ def die():
     # global die_roll
       die_roll=random.randrange(1,6) # die rolls it gives random number
       return die_roll
-
-
 def chance():
       nls=random.randrange(3)
       return nls
-n=chance()
-if n==0:
-    pos=pos+die()
-    print(pos)
-elif n==1:
-    pos=pos+die()
-    print(pos)
-else:
-     pos-=die()
-     print(pos)
+# count =0
+while pos<100:
+    n=chance()
+    if n==0:
+        pos=pos+die()
+        print(pos)
+    elif n==1:
+        pos=pos+die()
+        print(pos)
+    else:
+        pos-=die()
+        print(pos)
 
-
-print("current position =",pos)
-
+    if pos==100:
+         print("player wins the game")
+    elif pos<0:
+        pos=0
+   
        
 
 
